5ecab975e411030e3fd5e0a990816fa357e9dcf4
wadashin <Shin.86.miso@outlook.com>
2022-05-15 12:25:23 +0900

    コード規約に基づいて記述を変更した

---
+++ Assets/myGame/Scripts/wada/PixelObjecDisplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perapera_Puroto
+{
+    public class PixelObjectDisplay : MonoBehaviour
+    {
+        private Vector3 cashPosition;
+        /// <summary>ドットを1ドットずつ動かす</summary>
+        void LateUpdate()
+        {
+            cashPosition = transform.localPosition;
+            //小数点以下四捨五入
+            transform.localPosition = new Vector3(
+                            Mathf.RoundToInt(cashPosition.x),
+                            Mathf.RoundToInt(cashPosition.y),
+                            Mathf.RoundToInt(cashPosition.z)
+                       );
+        }
+
+        void OnRenderObject()
+        {
+            transform.localPosition = cashPosition;
+        }
+    }
+}
