504de7c23c92de687c74720bcbd28b6e378d3774
sorude26 <sorude26@gmail.com>
2022-06-12 13:38:03 +0900

    オブジェクトプール機能スクリプトを追加した

---
+++ Assets/myGame/Scripts/takeuchi/ObjectPoolManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    private const int DEFAULT_POOL_COUNT = 10;
+    private static ObjectPoolManager instance;
+    public static ObjectPoolManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var poolObj = new GameObject("PoolBase");
+                instance = poolObj.AddComponent<ObjectPoolManager>();
+                instance._keysDic = new Dictionary<string, int>();
+                instance._objectDic = new Dictionary<int, List<GameObject>>();
+                DontDestroyOnLoad(poolObj);
+            }
+            return instance;
+        }
+    }
+    private Dictionary<string, int> _keysDic = default;
+    private Dictionary<int, List<GameObject>> _objectDic = default;
+    public void CreatePool(GameObject poolObject)
+    {
+        if (_keysDic.ContainsKey(poolObject.name))
+        {
+            return;
+        }
+        List<GameObject> list = new List<GameObject>();
+        for (int i = 0; i < DEFAULT_POOL_COUNT; i++)
+        {
+            var obj = Instantiate(poolObject, this.transform);
+            obj.SetActive(false);
+            list.Add(obj);
+        }
+        _objectDic.Add(_keysDic.Count, list);
+        _keysDic.Add(poolObject.name, _keysDic.Count);
+    }
+    public GameObject Use(GameObject useObject)
+    {
+        if (!_keysDic.ContainsKey(useObject.name))
+        {
+            CreatePool(useObject);
+        }
+        foreach (var listObj in _objectDic[_keysDic[useObject.name]])
+        {
+            if (listObj.activeInHierarchy)
+            {
+                continue;
+            }
+            listObj.SetActive(true);
+            return listObj;
+        }
+        var obj = Instantiate(useObject, this.transform);
+        _objectDic[_keysDic[useObject.name]].Add(obj);
+        return obj;
+    }
+    public GameObject Use(GameObject useObject, Vector3 pos)
+    {
+        var obj = Use(useObject);
+        obj.transform.position = pos;
+        return obj;
+    }
+}
