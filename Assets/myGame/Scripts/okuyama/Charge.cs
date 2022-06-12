9c03cad4e4e576917e75a5752982a28e59fcd530
Nana_77 <nana77amery@gmail.com>
2022-06-12 18:00:27 +0900

    チャージ機能の調整

--- Assets/myGame/Scripts/okuyama/Charge.cs
+++ Assets/myGame/Scripts/okuyama/Charge.cs
@@ -5,11 +5,20 @@ using UnityEngine.Events;
 
 public class Charge : MonoBehaviour
 {
-    [SerializeField, Tooltip("チャージ中に武器を使えなくする")] UnityEvent _chargeing;
-    [SerializeField, Tooltip("チャージ完了時に武器を使えるようにする")] UnityEvent _chargeEnd;
-    [Tooltip("チャージまでに使える時間")] float _chargeTimer = 5.0f;
-    [Tooltip("チャージの最大値")] float _chargeMax = 10;
-    [Tooltip("発射できるか")] bool _chargebool = false;
+    [SerializeField, Tooltip("チャージ中に武器を使えなくする")] 
+    UnityEvent _chargeing;
+    [SerializeField, Tooltip("チャージ完了時に武器を使えるようにする")] 
+    UnityEvent _chargeEnd;
+    [Tooltip("チャージまでに使える時間")] 
+    float _chargeTimer = 5.0f;
+    [Tooltip("チャージの最大値")] 
+    float _chargeMax = 10;
+    [Tooltip("発射できるか")] 
+    bool _chargebool = false;
+    [Tooltip("チャージスピード")] 
+    float _chargeSpeed;
+    [Tooltip("チャージからの減少量")] 
+    float _decrease;
 
     void Start()
     {
@@ -23,16 +32,21 @@ public class Charge : MonoBehaviour
         {
             Chargeing();
         }
-        if (Input.GetKeyUp(KeyCode.Q))//チャージ開始
+        if (Input.GetKeyUp(KeyCode.Q))//チャージボタンを離したとき
         {
-            _chargebool = false;
-            _chargeEnd.Invoke();
+            UpCharge();
         }
+    }
+
+    private void UpCharge()//チャージボタンを離したとき
+    {
+        _chargebool = false;
+        _chargeEnd.Invoke();
     }
 
     private void Chargeing()//チャージがないとき
     {
-        _chargeTimer += Time.deltaTime;
+        _chargeTimer += _chargeSpeed * Time.deltaTime;
         if (_chargeTimer > _chargeMax)
         {
             Debug.Log($"チャージマックス{_chargeTimer}");
@@ -41,12 +55,11 @@ public class Charge : MonoBehaviour
             _chargeEnd.Invoke();
         }
     }
-
     private void ChargeMax()//チャージがあるとき
     {
         if (_chargeTimer > 0 && _chargebool == false)
         {
-            _chargeTimer -= Time.deltaTime;
+            _chargeTimer -= _decrease;
             if (_chargeTimer <= 0)
             {
                 _chargeing.Invoke();
