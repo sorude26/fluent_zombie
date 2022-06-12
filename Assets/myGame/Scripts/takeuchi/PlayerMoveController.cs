a1ae28f7026dfc245b3426ea6594977a4db71be5
sorude26 <sorude26@gmail.com>
2022-06-12 12:07:37 +0900

    カメラのテスト配置を行った

--- Assets/myGame/Scripts/takeuchi/PlayerMoveController.cs
+++ Assets/myGame/Scripts/takeuchi/PlayerMoveController.cs
@@ -8,6 +8,7 @@ public class PlayerMoveController : MonoBehaviour
     [SerializeField]
     private float _moveSpeed = 2f;
     private Rigidbody _rigidbody;
+    private float _g = -0.5f;
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -15,7 +16,7 @@ public class PlayerMoveController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 dir = (Vector3.right * PlayerInput.InputVector.x + Vector3.forward * PlayerInput.InputVector.y).normalized * _moveSpeed;
-        dir.y = _rigidbody.velocity.y;
+        dir.y = _rigidbody.velocity.y + _g;
         _rigidbody.velocity = dir;
     }
 }
