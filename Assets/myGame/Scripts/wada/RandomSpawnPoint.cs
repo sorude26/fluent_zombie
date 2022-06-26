fd2f1cace6e9e9fab9865f23a0bb7ef0793c61c4
wadashin <Shin.86.miso@outlook.com>
2022-06-26 16:07:34 +0900

    ランダムスポーンを作った

--- Assets/myGame/Scripts/wada/RandomSpawnPoint.cs
+++ Assets/myGame/Scripts/wada/RandomSpawnPoint.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class RandomSpawnPoint : MonoBehaviour
 {
-    LineRenderer line;
-
-
     // Start is called before the first frame update
     [SerializeField]
     float _xMax = 0;
@@ -19,53 +16,6 @@ public class RandomSpawnPoint : MonoBehaviour
 
     [SerializeField]
     float _yPos = 0;
-
-    [SerializeField] GameObject a;
-
-    [SerializeField] GameObject one;
-    [SerializeField] GameObject two;
-    [SerializeField] GameObject three;
-    [SerializeField] GameObject four;
-
-    void Start()
-    {
-        line = one.GetComponent<LineRenderer>();
-
-        a.transform.position = SpawnPos();
-        StartCoroutine("A");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        one.transform.position = new Vector3(_xMax,_yPos, _zMax);
-        two.transform.position = new Vector3(_xMax, _yPos, _zMin);
-        three.transform.position = new Vector3(_xMin, _yPos, _zMin);
-        four.transform.position = new Vector3(_xMin, _yPos, _zMax);
-
-        //line.SetPosition(0, one.transform.position);
-        //line.SetPosition(1, two.transform.position);
-        //line.SetPosition(2, three.transform.position);
-        //line.SetPosition(3, one.transform.position);
-
-
-        var positions = new Vector3[]{
-        one.transform.position,               // 開始点
-        two.transform.position,               // 終了点
-        three.transform.position,
-        four.transform.position,
-        one.transform.position,
-    };
-
-        // 線を引く場所を指定する
-        line.SetPositions(positions);
-    }
-    IEnumerator A()
-    {
-        a.transform.position = SpawnPos();
-        yield return new WaitForSeconds(0.2f);
-        StartCoroutine("A");
-    }
 
     private Vector3 SpawnPos()
     {
