c321f4b443948cc2970ddbfc5a4d58dab279a63a
sorude26 <sorude26@gmail.com>
2022-06-26 14:30:59 +0900

    武器関係の操作設定を追加し、テスト用配置を行った

--- Assets/myGame/Scripts/takeuchi/PlayerMoveController.cs
+++ Assets/myGame/Scripts/takeuchi/PlayerMoveController.cs
@@ -7,6 +7,8 @@ public class PlayerMoveController : MonoBehaviour
 {
     [SerializeField]
     private float _moveSpeed = 2f;
+    [SerializeField]
+    private Transform _body = default;
     private Rigidbody _rigidbody;
     private float _g = -0.5f;
     private void Start()
@@ -16,6 +18,10 @@ public class PlayerMoveController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 dir = (Vector3.right * PlayerInput.InputVector.x + Vector3.forward * PlayerInput.InputVector.y).normalized * _moveSpeed;
+        if (dir != Vector3.zero && _body != null)
+        {
+            _body.forward = dir;
+        }
         dir.y = _rigidbody.velocity.y + _g;
         _rigidbody.velocity = dir;
     }
